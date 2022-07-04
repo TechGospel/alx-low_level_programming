5dfe30a92ddb0ecc2b1f5287989ccf16ef9296c4
TechGospel <faithadeoti21@gmail>
2022-07-04 16:27:16 -0700

    Added a C program that prints the datatypes of different variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-	printf("Size of a float: %lu byte(s)", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(d));
 	return (0);
 }
 
