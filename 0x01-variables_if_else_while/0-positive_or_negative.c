68168c4ccac8e439093c7038933062c5b5d4da5e
TechGospel <faithadeoti21@gmail>
2022-07-04 14:24:32 -0700

    Added c program to check if a random number is positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,17 +10,16 @@
  */
 int main(void)
 {
- int n;
+	int n;
 
- srand(time(0));
- n = rand() - RAND_MAX / 2;
- /* your code goes there */
- if (n > 0)
-  printf("%d is positive\n", n);
- else if (n < 0)
-  printf("%d is negative\n", n);
- else
-  printf("%d is zero\n", n);
-
- return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+       	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
 }
